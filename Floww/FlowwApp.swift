c7c527fa4ba06e23b8d949dbf79f017bb8be8657
Pete Morris <peter.morris@marks-and-spencer.com>
2023-02-18 00:59:39 +0000

    Add Xcode project

---
+++ Floww/FlowwApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowwApp.swift
+//  Floww
+//
+//  Created by Morris, Peter on 18/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct FlowwApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
